6d417bca43f63b3d361c133e739b793b5aa97518
karthicks1409 <skarthickdk@gmail.com>
2025-05-23 15:57:37 +0530

    day11

---
+++ day11/lcm.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int gcd(int a,int b)
+{
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+int lcm(int x,int y)
+{
+    return (x*y)/gcd(x,y);
+}
+    int main()
+    {
+    printf("%d", lcm(45,20));
+    return 0;
+       
+}
